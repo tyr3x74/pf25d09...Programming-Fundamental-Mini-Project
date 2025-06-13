4c7bccfb50b7afae989bb6d8f4402cbaaaa1af6b
Gus Yudha <gyudha497@gmail.com>
2025-06-13 21:00:21 +0700

    commit

--- Graphical_TicTacToe/Connect.java
+++ Graphical_TicTacToe/Connect.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Locale;
 
-public class MySqlExample {
+public class Connect {
     public static void main(String[] args) throws ClassNotFoundException {
         String host, port, databaseName, userName, password;
         host = port = databaseName = userName = password = null;
