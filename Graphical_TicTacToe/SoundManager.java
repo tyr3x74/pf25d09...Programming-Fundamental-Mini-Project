9afa4794870ef45444f18592a9a39cf55ae3fa81
Shakila <shakiladamarista@gmail.com>
2025-06-25 10:52:27 +0700

    edit soundmanager

--- Graphical_TicTacToe/SoundManager.java
+++ Graphical_TicTacToe/SoundManager.java
@@ -6,35 +6,16 @@ import javax.sound.sampled.Clip;
 import java.io.File;
 
 public class SoundManager {
-    private Clip backgroundMusic;
 
-    public static void playSound(String filepath){
+    public static void playSound(String filepath) {
         try {
             AudioInputStream audio = AudioSystem.getAudioInputStream(new File(filepath));
             Clip clip = AudioSystem.getClip();
             clip.open(audio);
             clip.start();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error playing sound:" + e.getMessage());
             e.printStackTrace();
-        }
-    }
-
-    public void playBackground(String filepath){
-        try {
-            AudioInputStream audio = AudioSystem.getAudioInputStream(new File(filepath));
-            backgroundMusic = AudioSystem.getClip();
-            backgroundMusic.open(audio);
-            backgroundMusic.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e){
-            System.out.println("Error playing background music:" + e.getMessage());
-        }
-    }
-
-    public void stopBackground(){
-        if (backgroundMusic != null && backgroundMusic.isRunning()) {
-            backgroundMusic.stop();
-            backgroundMusic.close();
         }
     }
 }
